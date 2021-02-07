8b3e1c0700c9bd9b420aff1266d35d558b175409
unknown <williamcaetano14@gmail.com>
2021-02-07 01:34:02 -0300

    :construction: Added logic to modal

--- components/Day.js
+++ components/Day.js
@@ -1,24 +1,53 @@
-import { FormControl, FormLabel, Modal, Flex, Input } from '@chakra-ui/react';
+import {
+  FormControl,
+  FormLabel,
+  Modal,
+  Flex,
+  Input,
+  useDisclosure,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  Button,
+  Center
+} from '@chakra-ui/react';
+import { useState } from 'react';
 
-export default function Day({ day, onClick, events = [] }) {
-  useD;
+export default function Day({ day, onClick }) {
+  const [events, setEvents] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  function onSubmit(e) {
+    e.preventDefault();
+    setEvents([
+      {
+        name: e.target.name
+      },
+      ...events
+    ]);
+  }
+
   return (
-    <>
-      <div>
-        <p>{day}</p>
-        {events.map((eventOfDay) => (
-          <button>{eventOfDay}</button>
-        ))}
-        <button>adicionar Evento</button>
-      </div>
-      <Modal>
-        <Flex as="form">
-          <FormControl>
-            <FormLabel>Nome do evento</FormLabel>
-            <Input name="event" />
-          </FormControl>
-        </Flex>
+    <div>
+      <p>{day}</p>
+      {events.map((eventOfDay) => (
+        <button>{eventOfDay.name}</button>
+      ))}
+      <button onClick={onOpen}>Adicionar evento</button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent margin="auto" w="300px" backgroundColor="white">
+          <ModalOverlay />
+          <ModalCloseButton />
+          <Center>
+            <Flex as="form" onSubmit={onSubmit} direction="column">
+              <FormControl>
+                <FormLabel>Nome do evento</FormLabel>
+                <Input name="event" />
+              </FormControl>
+              <Button type="submit">Criar</Button>
+            </Flex>
+          </Center>
+        </ModalContent>
       </Modal>
-    </>
+    </div>
   );
 }
